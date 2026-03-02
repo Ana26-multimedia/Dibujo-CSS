9f431e08389ef378727eb629bdc1789542e12cca
Ana26-multimedia <a.acunac@uniandes.edu.co>
2026-03-02 16:47:25 +0000

    Script

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("EYYYYY Helloo");
+
+const donpepito=document.getElementById("donpepito");
+
+donpepito.addEventListener("click", ()=> {
+    
+    donpepito.innerText = "Entusiasmo"
+    donpepito.style.backgroundColor= "yellow"
+})
