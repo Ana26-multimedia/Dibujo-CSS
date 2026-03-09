0553d1459f923ef947e4e2e722af15c81bfc2d50
Ana26-multimedia <a.acunac@uniandes.edu.co>
2026-03-09 15:57:37 +0000

    una historia muy bonita

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 console.log("EYYYYY Helloo");
 
-const donpepito=document.getElementById("donpepito");
+const donpepito = document.getElementById("donpepito");
+const dialogo = document.getElementById("texto");
+
+const listatextos = [ "Hola Don pepito", "Hellooo Don jose", "Pase usted por mi casa" ,"por su casa yo pase" ,"vio usted a mi abuela" ,"a su abuela yo la vi","adios don pepito", "adios don jose"];
+
+
+let estadoinicial = true;
+let indice = 0;
 
 donpepito.addEventListener("click", ()=> {
-    
-    donpepito.innerText = "Entusiasmo"
-    donpepito.style.backgroundColor= "yellow"
+    if(estadoinicial === true){ 
+          donpepito.innerText = "Entusiasmo";
+    donpepito.style.backgroundColor= "yellow"; 
+
+        
+     } else {
+    donpepito.innerText = "Don pepito";
+    donpepito.style.backgroundColor= "pink";
+     }
+estadoinicial = !estadoinicial;
+
 })
+
+
+dialogo.addEventListener("click", () => { 
+    if (indice < listatextos.length) { 
+        dialogo.innerText = listatextos[indice];
+        dialogo.style.color = "black";
+        indice = indice +1; 
+        }
+        else { 
+            indice = 0;
+        }
+})
+
